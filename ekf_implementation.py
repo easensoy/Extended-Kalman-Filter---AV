99d2606feb8d12007ace8d1ac577ceeaef79caa4
Emin Alper Şensoy <76905667+easensoy@users.noreply.github.com>
2023-06-19 14:57:15 +0300

    EKF estimator for full self-driving vehicles

--- ekf_implementation.py
+++ ekf_implementation.py
@@ -1,7 +1,6 @@
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-
 '''
 #To see what the pickle file contains do the following
 objects = []
@@ -15,7 +14,7 @@ with (open('data/data.pickle', 'rb')) as openfile:
 print(objects)
 '''
 
-with open('data.pickle', 'rb') as f:
+with open('extracted_data.pickle', 'rb') as f:
     data = pickle.load(f)
 
 t = data['t']  # timestamps [s]
@@ -46,8 +45,8 @@ r_var = 0.01
 #b_var = 0.1  # bearing measurement variance - pickle gives a value of 0.0005
 b_var = 10
 
-Q_km = np.diag([v_var, om_var]) # input noise covariance 
-cov_y = np.diag([r_var, b_var])  # measurement noise covariance 
+Q_km = np.diag([v_var, om_var]) # input noise covariance
+cov_y = np.diag([r_var, b_var])  # measurement noise covariance
 
 x_est = np.zeros([len(v), 3])  # estimated states, x, y, and theta
 P_est = np.zeros([len(v), 3, 3])  # state covariance matrices
@@ -69,7 +68,7 @@ def wraptopi(x):
 
 
 def measurement_update(lk, rk, bk, P_check, x_check):
-    
+
     # 1. Compute measurement Jacobian
     x = x_check[0]
     y = x_check[1]
@@ -79,54 +78,54 @@ def measurement_update(lk, rk, bk, P_check, x_check):
     print("Landmark coordinates:", lk)
     lx = lk[0]
     ly = lk[1]
- 
+
     #calculate expected range measurement
     #d is the distance between robot center and laser rangefinder [m]
     print("d = ", d)
     d_x = lx - x - d*np.cos(th)
     d_y = ly - y - d*np.sin(th)
-    frac = d_x**2 + d_y**2  
+    frac = d_x**2 + d_y**2
     range_exp = np.sqrt( frac )
-    
-    
+
+
     H_k = np.array([[-d_x/range_exp, -d_y/range_exp, d * (d_x*np.sin(th) - d_y*np.cos(th))/range_exp],
                   [d_y/frac, -d_x/frac, -1 - d * (np.sin(th)*d_y + np.cos(th)*d_x)/frac]])
     H_k = H_k.reshape(2, 3)
-    
+
     M_k = np.identity(2)
 
-    
+
     print("H_k.shape = ", H_k.shape)
     print("H_k = \n", H_k)
     print("M_k.shape = ", M_k.shape)
     print("P_check.shape = ", P_check.shape)
     print("cov_y.shape = ", cov_y.shape)
-    
-    
+
+
     # 2. Compute Kalman Gain. Here is a 3x2 array
     K_k = P_check @ H_k.T @ np.linalg.inv(H_k @ P_check @ H_k.T + M_k @ cov_y @ M_k.T)
     print("K_k.shape = ", K_k.shape)
-    
+
     # 3. Correct predicted state (remember to wrap the angles to [-pi,pi])
     phi = np.arctan2(d_y, d_x) - th
     y_k = np.array([[range_exp], [wraptopi(phi)]])
     y_k = y_k.reshape(2,1)
     y_measured = np.array([[rk], [wraptopi(bk)]])
-    
+
     print("y_k.shape = ", y_k.shape)
     print("y_measured.shape = ", y_measured.shape)
 
     x_check = x_check + K_k @ (y_measured - y_k)
-    
+
     print("x_check = \n", x_check)
     print("x_check.shape = ", x_check.shape)
-    
+
     x_check[2] = wraptopi(x_check[2])
 
 
     # 4. Correct covariance
     P_check = (np.identity(3) - K_k @ H_k) @ P_check
-    
+
     '''  '''
     return x_check, P_check
 
@@ -140,8 +139,8 @@ for k in range(1, len(t)):  # start at 1 because we've set the initial predicito
     delta_t = t[k] - t[k - 1]  # time step (difference between timestamps)
 
     # 1. Update state with odometry readings (remember to wrap the angles to [-pi,pi])
-    
-    
+
+
     #if it is the first time we run this loop, we use the initial state value
     if (flag_initial == 1):
         #copy initial state values by taking first row
@@ -149,7 +148,7 @@ for k in range(1, len(t)):  # start at 1 because we've set the initial predicito
 
         #copy initial covariance by taking first row
         P_check = P_est[0]
-        
+
         flag_initial=0
 
 
@@ -157,22 +156,22 @@ for k in range(1, len(t)):  # start at 1 because we've set the initial predicito
     print(x_check.shape)
     theta = x_check[2]
     print(theta)
-    
-    
+
+
     #trying to create the motion model function
-    A1 = np.array([[np.cos(wraptopi(theta)), 0], [np.sin(wraptopi(theta)), 0], [0, 1]], dtype='float')  
+    A1 = np.array([[np.cos(wraptopi(theta)), 0], [np.sin(wraptopi(theta)), 0], [0, 1]], dtype='float')
     A2 = np.array([[v[k-1]], [om[k-1]]])
-    
+
     print("A1.shape = ", A1.shape)
     print("A2.shape = ", A2.shape)
-    
+
     x_check = x_check + delta_t * np.matmul(A1, A2)
     x_check[2] = wraptopi(x_check[2])
-    
-    
+
+
     print("x_check = \n", x_check)
     print("x_check.shape = ", x_check.shape)
-        
+
 
     # 2. Motion model jacobian with respect to last state
     # Our state funtion takes a 3 dimensional vector (x, y, theta) and spits out
@@ -180,30 +179,30 @@ for k in range(1, len(t)):  # start at 1 because we've set the initial predicito
     F_km = np.zeros([3, 3])
     F_km = np.array([[1, 0, -1 * delta_t * v[k-1] * np.sin(wraptopi(theta))], [0, 1, delta_t * v[k-1] * np.cos(wraptopi(theta))], [0, 0, 1]], dtype='float')
     print(F_km)
- 
+
 
     # 3. Motion model jacobian with respect to noise
     L_km = np.zeros([3, 2])
     L_km = delta_t * A1
     print(L_km)
-    
-    
+
+
     # 4. Propagate uncertainty
     P_check = F_km @ P_check @ F_km.T + L_km @ Q_km @ L_km.T
     print(P_check)
-    
+
 
     # 5. Update state estimate using available landmark measurements
     for i in range(len(r[k])):
         x_check, P_check = measurement_update(l[i], r[k, i], b[k, i], P_check, x_check)
-   
-    
+
+
     # Set final state predictions for timestep
     x_est[k, 0] = x_check[0]
     x_est[k, 1] = x_check[1]
     x_est[k, 2] = x_check[2]
     P_est[k, :, :] = P_check
-    
+
 
 e_fig = plt.figure()
 ax = e_fig.add_subplot(111)
